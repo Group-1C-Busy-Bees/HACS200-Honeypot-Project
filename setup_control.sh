425f9ed6173cc0120652a460f627cf642e100d71
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-08 15:59:45 -0400

    added logging

--- setup_control.sh
+++ setup_control.sh
@@ -4,9 +4,13 @@
 if [[ $# -ne 1 ]];
 then
 	echo "Usage: ./setup_control <Container Name>"
-	exit 1
+	echo "ERROR: incorrect params in $(pwd)/setup_control.sh (5)" >> scripts.log
+	exit 5
 fi
 
 container_name=$1
 
-sudo lxc-attach -- bash -c "mkdir Directory1 Directory2 Directory3 | cd Directory1| mkdir Directory4 | cd ../Directory2 | mkdir Directory5 | cd ../Directory3 | mkdir Directory6 Directory7"
+sudo lxc-attach -n "$container_name" -- bash -c "mkdir Directory1 Directory2 Directory3 | cd Directory1| mkdir Directory4 | cd ../Directory2 | mkdir Directory5 | cd ../Directory3 | mkdir Directory6 Directory7"
+
+echo "SUCCESS: $(pwd)/setup_control.sh (0)" >> scripts.log
+exit 0
