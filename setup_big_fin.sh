aa05e2dfce606afdac026860d80f059911054668
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-08 15:37:14 -0400

    added logging feature

--- setup_big_fin.sh
+++ setup_big_fin.sh
@@ -3,15 +3,19 @@
 if [[ $# -ne 1 ]];
 then
 	echo "Usage: ./setup_big_fin <Container Name>"
-	exit 1
+ 	echo "ERROR: incorrect params in $(pwd)/setup_big_fin.sh (2)" >> scripts.log
+	exit 2
 fi
 
 container_name=$1
 
-sudo lxc-attach -- bash -c "mkdir Customer_Service Services Activity | cd Customer_Service | mkdir Resources | cd ../Services | mkdir Credit_Card Retirement | cd ../Activity | mkdir Transfers"
+sudo lxc-attach -n "$container_name" -- bash -c "mkdir Customer_Service Services Activity | cd Customer_Service | mkdir Resources | cd ../Services | mkdir Credit_Card Retirement | cd ../Activity | mkdir Transfers"
 
-sudo lxc file push big_bankacc_customer.csv $container_name/root/Customer_Service/bankacc_customer.csv
+sudo lxc file push big_bankacc_customer.csv "$container_name"/root/Customer_Service/bankacc_customer.csv
 
-sudo lxc file push big_creditcard_info.csv $container_name/root/Services/Credit_Card/creditcard_info.csv 
+sudo lxc file push big_creditcard_info.csv "$container_name"/root/Services/Credit_Card/creditcard_info.csv 
 
-sudo lxc file push big_moneytransfer_hist.xls $container_name/root/Activity/Transfers/moneytransfer_hist.xls
+sudo lxc file push big_moneytransfer_hist.xls "$container_name"/root/Activity/Transfers/moneytransfer_hist.xls
+
+echo "SUCCESS: $(pwd)/setup_big_fin.sh (0)" >> scripts.log
+exit 0
