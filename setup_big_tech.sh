5ab3e21d2d4bec7799514fd82e556fa2794dad9f
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-08 15:55:58 -0400

    added logging

--- setup_big_tech.sh
+++ setup_big_tech.sh
@@ -2,15 +2,18 @@
 # Catching incorrect params for set up command
 if [[ $# -ne 1 ]];
 then
-	echo "Usage: ./setup_big_tech <Container Name>"
-	exit 1
+ 	echo "ERROR: incorrect params in $(pwd)/setup_big_tech.sh (4)" >> scripts.log
+	exit 4
 fi
 
 container_name=$1
 
-sudo lxc-attach -- bash -c "mkdir Hardware Website Customer_Services | cd Hardware | mkdir Home_Devices | cd ../Website | mkdir Resources | cd ../Customer_Services | mkdir Guides Assistance"
+sudo lxc-attach -n "$container_name" -- bash -c "mkdir Hardware Website Customer_Services | cd Hardware | mkdir Home_Devices | cd ../Website | mkdir Resources | cd ../Customer_Services | mkdir Guides Assistance"
 
-sudo lxc file push big_device_data.csv $container_name/root/Hardware/device_data.csv
+sudo lxc file push big_device_data.csv "$container_name"/root/Hardware/device_data.csv
 
-sudo lxc file push big_userprefs.xls $container_name/root/Website/Resources/userprefs.xls
-sudo lxc file push big_login_customer.csv $container_name/root/Customer_Services/Assistance/login_customer.csv
+sudo lxc file push big_userprefs.xls "$container_name"/root/Website/Resources/userprefs.xls
+sudo lxc file push big_login_customer.csv "$container_name"/root/Customer_Services/Assistance/login_customer.csv
+
+echo "SUCCESS: $(pwd)/setup_big_tech.sh (0)" >> scripts.log
+exit 0
