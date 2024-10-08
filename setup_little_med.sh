cd00abbc048a1f9a562d4a4fdf08f504e9366e51
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-08 16:07:47 -0400

    added logging

--- setup_little_med.sh
+++ setup_little_med.sh
@@ -3,15 +3,19 @@
 if [[ $# -ne 1 ]];
 then
 	echo "Usage: ./setup_little_med <Container Name>"
-	exit 1
+ 	echo "ERROR: incorrect params in $(pwd)/setup_little_med.sh (8)" >> scripts.log
+	exit 8
 fi
 
 container_name=$1
 
-sudo lxc-attach -- bash -c "mkdir Patients General Legal | cd Patients | mkdir Data | cd ../General | mkdir Historical | cd ../Public | mkdir Information Documents"
+sudo lxc-attach -n "$container_name" -- bash -c "mkdir Patients General Legal | cd Patients | mkdir Data | cd ../General | mkdir Historical | cd ../Public | mkdir Information Documents"
 
-sudo lxc file push little_patient_files.csv $container_name/root/Patients/Data/patient_files.csv
+sudo lxc file push little_patient_files.csv "$container_name"/root/Patients/Data/patient_files.csv
 
-sudo lxc file push little_hospital_hist.xls $container_name/root/General/Historical/hospital_hist.xls
+sudo lxc file push little_hospital_hist.xls "$container_name"/root/General/Historical/hospital_hist.xls
 
-sudo lxc file push little_insurance.csv $container_name/root/Legal/Information/insurance.csv
+sudo lxc file push little_insurance.csv "$container_name"/root/Legal/Information/insurance.csv
+
+echo "SUCCESS: $(pwd)/setup_little_med.sh (0)" >> scripts.log
+exit 0
