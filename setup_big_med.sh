6c929c0554f9dd0e95855938ff6c56c691a03ba9
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-08 15:51:24 -0400

    added logging

--- setup_big_med.sh
+++ setup_big_med.sh
@@ -2,16 +2,19 @@
 # Catching incorrect params for set up command
 if [[ $# -ne 1 ]];
 then
-	echo "Usage: ./setup_big_med <Container Name>"
-	exit 1
+	echo "ERROR: incorrect params in $(pwd)/setup_big_med.sh (3)" >> scripts.log
+	exit 3
 fi
 
 container_name=$1
 
-sudo lxc-attach -- bash -c "mkdir Patients General Legal | cd Patients | mkdir Data | cd ../General | mkdir Historical | cd ../Public | mkdir Information Documents"
+sudo lxc-attach -n "$container_name" -- bash -c "mkdir Patients General Legal | cd Patients | mkdir Data | cd ../General | mkdir Historical | cd ../Public | mkdir Information Documents"
 
-sudo lxc file push big_patient_files.csv $container_name/root/Patients/Data/patient_files.csv
+sudo lxc file push big_patient_files.csv "$container_name"/root/Patients/Data/patient_files.csv
 
-sudo lxc file push big_hospital_hist.xls $container_name/root/General/Historical/hospital_hist.xls
+sudo lxc file push big_hospital_hist.xls "$container_name"/root/General/Historical/hospital_hist.xls
 
-sudo lxc file push big_insurance.csv $container_name/root/Legal/Information/insurance.csv
+sudo lxc file push big_insurance.csv "$container_name"/root/Legal/Information/insurance.csv
+
+echo "SUCCESS: $(pwd)/setup_big_med.sh (0)" >> scripts.log
+exit 0
