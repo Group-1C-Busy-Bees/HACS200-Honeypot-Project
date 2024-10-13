4f9f2bf91ed921ac64bc705dc4930197fa0b0c97
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-13 01:45:28 -0400

    updated log feature

--- setup_control.sh
+++ setup_control.sh
@@ -3,8 +3,7 @@
 # Catching incorrect params for set up command
 if [[ $# -ne 1 ]];
 then
-	echo "Usage: ./setup_control <Container Name>"
-	echo "ERROR: incorrect params in $(pwd)/setup_control.sh (5)" >> scripts.log
+	echo "[$(date +'%Y-%m-%d %H:%M:%S')] CRITICAL ERROR: incorrect params in $(pwd)/setup_control.sh (5)" >> scripts.log
 	exit 5
 fi
 
@@ -12,5 +11,5 @@ container_name=$1
 
 sudo lxc-attach -n "$container_name" -- bash -c "mkdir Directory1 Directory2 Directory3 | cd Directory1| mkdir Directory4 | cd ../Directory2 | mkdir Directory5 | cd ../Directory3 | mkdir Directory6 Directory7"
 
-echo "SUCCESS: $(pwd)/setup_control.sh (0)" >> scripts.log
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: $(pwd)/setup_control.sh completed (0)" >> scripts.log
 exit 0
