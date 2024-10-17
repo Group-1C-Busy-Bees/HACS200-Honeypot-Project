1aa920dc8bd1f145c0777af24fdeb7f478203633
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-17 16:05:46 -0400

    it wokrs now

--- Directory Setup/setup_big_fin.sh
+++ Directory Setup/setup_big_fin.sh
@@ -8,8 +8,9 @@ fi
 
 CONTAINER_NAME="$1"
 
-sudo lxc-attach -n "$CONTAINER_NAME" -- bash -c "mkdir Customer_Service Services Activity | cd Customer_Service | mkdir Resources | cd ../Services | mkdir Credit_Card Retirement | cd ../Activity | mkdir Transfers"
+sudo lxc-attach -n "$CONTAINER_NAME" -- bash -c "mkdir Customer_Service Services Activity ; cd Customer_Service ; mkdir Resources ; cd ../Services ; mkdir Credit_Card Retirement ; cd ../Activity ; mkdir Transfers"
 
+# THE FOLLOWING COMMANDS NEED SUPERUSER (SU) PERMS 
 cp big_bankacc_customer.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/bankacc_customer.csv
 
 cp big_creditcard_info.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/creditcard_info.csv 
