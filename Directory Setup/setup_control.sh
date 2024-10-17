5acbc557d354930989904821239bee308a5077c9
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-17 16:15:27 -0400

    pls say it works

--- Directory Setup/setup_control.sh
+++ Directory Setup/setup_control.sh
@@ -9,7 +9,7 @@ fi
 
 container_name=$1
 
-sudo lxc-attach -n "$container_name" -- bash -c "mkdir Directory1 Directory2 Directory3 | cd Directory1| mkdir Directory4 | cd ../Directory2 | mkdir Directory5 | cd ../Directory3 | mkdir Directory6 Directory7"
+sudo lxc-attach -n "$container_name" -- bash -c "mkdir Directory1 Directory2 Directory3; cd Directory1; mkdir Directory4; cd ../Directory2; mkdir Directory5; cd ../Directory3; mkdir Directory6 Directory7"
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: $(pwd)/setup_control.sh completed (0)" >> scripts.log
 exit 0
