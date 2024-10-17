902174d390f5f5049fc4daf31598d76afe157c7f
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-17 16:14:28 -0400

    PLS SAY IT WORKS

--- Directory Setup/setup_big_tech.sh
+++ Directory Setup/setup_big_tech.sh
@@ -8,12 +8,13 @@ fi
 
 container_name=$1
 
-sudo lxc-attach -n "$container_name" -- bash -c "mkdir Hardware Website Customer_Services | cd Hardware | mkdir Home_Devices | cd ../Website | mkdir Resources | cd ../Customer_Services | mkdir Guides Assistance"
+sudo lxc-attach -n "$container_name" -- bash -c "mkdir Hardware Website Customer_Services; cd Hardware; mkdir Home_Devices; cd ../Website; mkdir Resources; cd ../Customer_Services; mkdir Guides Assistance"
 
-sudo lxc file push big_device_data.csv "$container_name"/root/Hardware/device_data.csv
+cp big_device_data.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home//Hardware/device_data.csv
 
-sudo lxc file push big_userprefs.xls "$container_name"/root/Website/Resources/userprefs.xls
-sudo lxc file push big_login_customer.csv "$container_name"/root/Customer_Services/Assistance/login_customer.csv
+cp big_userprefs.xls /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/Website/Resources/userprefs.xls
+
+cp big_login_customer.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/Customer_Services/Assistance/login_customer.csv
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: $(pwd)/setup_big_tech.sh completed (0)" >> scripts.log
 exit 0
