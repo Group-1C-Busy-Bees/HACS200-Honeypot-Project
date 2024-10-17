457e99d8a3f90011a05dc885e4499bbb2a10f9fc
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-17 15:45:03 -0400

    praying for ourselves

--- Directory Setup/setup_big_fin.sh
+++ Directory Setup/setup_big_fin.sh
@@ -6,15 +6,15 @@ then
 	exit 2
 fi
 
-container_name=$1
+CONTAINER_NAME="$1"
 
-sudo lxc-attach -n "$container_name" -- bash -c "mkdir Customer_Service Services Activity | cd Customer_Service | mkdir Resources | cd ../Services | mkdir Credit_Card Retirement | cd ../Activity | mkdir Transfers"
+sudo lxc-attach -n "$CONTAINER_NAME" -- bash -c "mkdir Customer_Service Services Activity | cd Customer_Service | mkdir Resources | cd ../Services | mkdir Credit_Card Retirement | cd ../Activity | mkdir Transfers"
 
-sudo lxc file push big_bankacc_customer.csv "$container_name"/root/Customer_Service/bankacc_customer.csv
+cp big_bankacc_customer.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/bankacc_customer.csv
 
-sudo lxc file push big_creditcard_info.csv "$container_name"/root/Services/Credit_Card/creditcard_info.csv 
+cp big_creditcard_info.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/creditcard_info.csv 
 
-sudo lxc file push big_moneytransfer_hist.xls "$container_name"/root/Activity/Transfers/moneytransfer_hist.xls
+cp big_moneytransfer_hist.xls /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/moneytransfer_hist.xls
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: $(pwd)/setup_big_fin.sh (0) complete" >> scripts.log
 exit 0
