e55e9d55a73ad22278a67aa149d8c9103002d252
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-17 16:28:25 -0400

    PLS SAY IT WORKS

--- Directory Setup/setup_little_med.sh
+++ Directory Setup/setup_little_med.sh
@@ -8,13 +8,13 @@ fi
 
 container_name=$1
 
-sudo lxc-attach -n "$container_name" -- bash -c "mkdir Patients General Legal | cd Patients | mkdir Data | cd ../General | mkdir Historical | cd ../Public | mkdir Information Documents"
+sudo lxc-attach -n "$container_name" -- bash -c "mkdir Patients General Legal; cd Patients; mkdir Data; cd ../General; mkdir Historical; cd ../Public; mkdir Information Documents"
 
-sudo lxc file push little_patient_files.csv "$container_name"/root/Patients/Data/patient_files.csv
+cp little_patient_files.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/Patients/Data/patient_files.csv
 
-sudo lxc file push little_hospital_hist.xls "$container_name"/root/General/Historical/hospital_hist.xls
+cp little_hospital_hist.xls /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/General/Historical/hospital_hist.xls
 
-sudo lxc file push little_insurance.csv "$container_name"/root/Legal/Information/insurance.csv
+cp little_insurance.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/Legal/Information/insurance.csv
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: $(pwd)/setup_little_med.sh completed (0)" >> scripts.log
 exit 0
