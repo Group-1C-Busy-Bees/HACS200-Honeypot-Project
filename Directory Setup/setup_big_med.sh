7e542c8f032768c74b68ddeb6d1aa7f8d03b9cd5
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-17 16:13:28 -0400

    PLS SAY IT WORKS

--- Directory Setup/setup_big_med.sh
+++ Directory Setup/setup_big_med.sh
@@ -6,15 +6,15 @@ then
 	exit 3
 fi
 
-container_name=$1
+CONTAINER_NAME=$1
 
-sudo lxc-attach -n "$container_name" -- bash -c "mkdir Patients General Legal | cd Patients | mkdir Data | cd ../General | mkdir Historical | cd ../Public | mkdir Information Documents"
+sudo lxc-attach -n "$CONTAINER_NAME" -- bash -c "mkdir Patients General Legal; cd Patients; mkdir Data; cd ../General; mkdir Historical; cd ../Public; mkdir Information Documents"
 
-sudo lxc file push big_patient_files.csv "$container_name"/root/Patients/Data/patient_files.csv
+cp big_patient_files.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/Patients/Data/patient_files.csv
 
-sudo lxc file push big_hospital_hist.xls "$container_name"/root/General/Historical/hospital_hist.xls
+cp big_hospital_hist.xls /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/General/Historical/hospital_hist.xls
 
-sudo lxc file push big_insurance.csv "$container_name"/root/Legal/Information/insurance.csv
+cp big_insurance.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/Legal/Information/insurance.csv
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: $(pwd)/setup_big_med.sh completed (0)" >> scripts.log
 exit 0
