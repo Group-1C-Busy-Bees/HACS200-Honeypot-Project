063464f436346f594aff2a86772a32b33be69c31
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-18 00:24:19 -0400

    fixed incorrect directory name

--- Directory Setup/setup_big_med.sh
+++ Directory Setup/setup_big_med.sh
@@ -8,7 +8,7 @@ fi
 
 CONTAINER_NAME=$1
 
-sudo lxc-attach -n "$CONTAINER_NAME" -- bash -c "mkdir Patients General Legal; cd Patients; mkdir Data; cd ../General; mkdir Historical; cd ../Public; mkdir Information Documents"
+sudo lxc-attach -n "$CONTAINER_NAME" -- bash -c "mkdir Patients General Public; cd Patients; mkdir Data; cd ../General; mkdir Historical; cd ../Public; mkdir Information Documents"
 
 cp big_patient_files.csv /var/lib/lxc/"$CONTAINER_NAME"/rootfs/home/Patients/Data/patient_files.csv
 
