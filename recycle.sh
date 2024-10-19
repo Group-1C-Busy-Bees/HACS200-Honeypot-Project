e52f37707fdadc51278d785a4a8d8a0ad28b7242
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-19 19:26:44 -0400

    updated nat rules, updated logic
    meow meow meow meow

--- recycle.sh
+++ recycle.sh
@@ -44,32 +44,31 @@ if [[ $LINE_COUNT -ge 1 ]]
 then
     # Output redirect so that the first line of the utility file contains: number of minutes to run container, idle time, container name, and start time of container
     echo ""$MAX_DURATION_TIME" "$IDLE_TIME" "$HP_CONFIG" $(date +%s)" > ./recycle_util_"$CONTAINER_NAME"
-    START_TIME=`date +%Y-%m-%dT%H:%M:%S%Z`
-    echo "[$(date +'%Y-%m-%d %H:%M:%S')] STATUS: "$CONTAINER_NAME" STARTED at $START_TIME" >> scripts.log
-
-    # set up NAT rules
-    sudo ip addr add "$EXTERNAL_IP"/16 brd + dev eth0
-    sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$EXTERNAL_IP" --jump DNAT --to-destination "$CONTAINER_IP"
-    sudo iptables --table nat --insert POSTROUTING --source "$CONTAINER_IP" --destination 0.0.0.0/0 --jump SNAT --to-source "$EXTERNAL_IP"
-    echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: nat rules set for "$CONTAINER_NAME"" >> scripts.log
+    START_TIME=$(date +%Y-%m-%dT%H:%M:%S%Z)
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] STATUS: "$CONTAINER_NAME" STARTED at "$START_TIME"" >> scripts.log
 
     # set-up MITM
-    DAY=`date +%Y-%m-%d`
-    sudo forever -l ~/attacker_logs/debug_logs/$HP_CONFIG/$START_TIME -a start ~/MITM/mitm.js -n "$CONTAINER_NAME" -i "$CONTAINER_IP" -p 32887 --auto-access --auto-access-fixed 2 --debug
+    sudo forever -l ~/attacker_logs/debug_logs/"$HP_CONFIG"/"$START_TIME" -a start ~/MITM/mitm.js -n "$CONTAINER_NAME" -i "$CONTAINER_IP" -p 32887 --auto-access --auto-access-fixed 2 --debug # does auto-access actually work
     sudo sysctl -w net.ipv4.conf.all.route_localnet=1
 
-    sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$EXTERNAL_IP" --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:32887
-    sudo ip addr add "$EXTERNAL_IP"/16 brd + dev eth0
+    # NAT rules for attacker to container (putting container online)
+    sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$EXTERNAL_IP" --jump DNAT --to-destination "$CONTAINER_IP"
+    sudo iptables --table nat --insert POSTROUTING --source "$CONTAINER_IP" --destination 0.0.0.0/0 --jump SNAT --to-source "$EXTERNAL_IP"
+    # housekeeping echo statement
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: nat rules set for "$CONTAINER_NAME"" >> scripts.log
+    # NAT rules for MITM
+    sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$EXTERNAL_IP" --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:64462 # incorrect destination (port number)???!?!?!
+    # housekeeping echo statement
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: set-up mitm on "$CONTAINER_NAME" in $(pwd)/recycle.sh" >> scripts.log
-    echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: $(pwd)/recycle.sh completed (0)" >> scripts.log
 
+    # while(true) until an attacker logs in
     while true; do
-        LOGIN_TIME=`grep "Opened shell for attacker" ~/attacker_logs/debug_logs/$HP_CONFIG/$START_TIME | cut -c 1-19`
-        if [ $LOGIN_TIME != "" ]
+        LOGIN_TIME=$(grep "Opened shell for attacker" ~/attacker_logs/debug_logs/$HP_CONFIG/$START_TIME | cut -c 1-19) # grepping for attacker log-in cue
+        if [ "$LOGIN_TIME" != "" ] # checking to see if attacker has connected to container
         then 
-            LOGIN_EPOCH=`date -d "$LOGIN_TIME" +"%s"`
-            echo " $LOGIN_EPOCH" >> ./recycle_util_$CONTAINER_NAME
-            break
+            LOGIN_EPOCH=$(date -d "$LOGIN_TIME" +"%s") # grabs log-in time
+            echo " "$LOGIN_EPOCH"" >> ./recycle_util_"$CONTAINER_NAME" # puts log=in time into util file
+            break 
         fi
     done
 
@@ -90,16 +89,20 @@ else # container is already up, does not need to be created
         # Check for logout
         LOGOUT=$(grep "Honeypot ended shell" ~/attacker_logs/$HP_CONFIG/$START_TIME | wc -l)
         
-        # Checking to see if it is time to recycle
-        if [[ $LOGOUT -eq 1 || $(($CURRENT_TIME - $LAST_ACTION_EPOCH)) -ge $(($IDLE_TIME * 60)) || $TIME_ELAPSED -ge $(($TARGET_DURATION * 60)) ]]
-            then # if it is time to recycle
-            # remove NAT rules
-            sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$EXTERNAL_IP" --jump DNAT --to-destination "$CONTAINER_IP"
+        # Checking to see if container needs to be recycled based on the following cases:
+        # attacker has been in honeypot for 10 minutes
+        # attacker has been idle for 2 minutes
+        # attacker has logged out
+        if [[ "$LOGOUT" -eq 1 || $(($CURRENT_TIME - $LAST_ACTION_EPOCH)) -ge $(($IDLE_TIME * 60)) || "$TIME_ELAPSED" -ge $(($TARGET_DURATION * 60)) ]]
+            then # if it is time to recycle...
+            # remove NAT rules for MITM
+            sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$EXTERNAL_IP" --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:64462 # incorrect destination (port number)???!?!?!
+            # remove NAT rules for container (take container offline)
             sudo iptables --table nat --delete POSTROUTING --source "$CONTAINER_IP" --destination 0.0.0.0/0 --jump SNAT --to-source "$EXTERNAL_IP"
-            sudo ip addr delete "$EXTERNAL_IP"/16 brd + dev eth0
+            sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$EXTERNAL_IP" --jump DNAT --to-destination "$CONTAINER_IP"
             # housekeeping echo statement
             echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: nat rules removed for "$CONTAINER_NAME"" >> scripts.log
-
+            
             # Stop old container
             sudo lxc-stop -n "$CONTAINER_NAME"
             # echo statement is for housekeeping
@@ -108,9 +111,8 @@ else # container is already up, does not need to be created
             sudo lxc-remove -n "$CONTAINER_NAME"
             # echo statement is for housekeeping
             echo "[$(date +'%Y-%m-%d %H:%M:%S')] STATUS: "$CONTAINER_NAME" REMOVED at $(date +%Y-%m-%dT%H:%M:%S%Z)" >> scripts.log  
-            # delete utility file
+            # delete associated utility file
             rm ./recycle_util_"$CONTAINER_NAME"
-            
             # manage logs (should be a script call)
             ./grab_logs $CONTAINER_NAME
             
@@ -119,10 +121,12 @@ else # container is already up, does not need to be created
             # start up container 
             sudo lxc-start -n "$CONTAINER_NAME"
             
-            # set-up stand-by container's NAT rules
-            sudo ip addr add "$EXTERNAL_IP"/16 brd + dev eth0
+            # set-up container NAT rules (putting container back online again)
             sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$EXTERNAL_IP" --jump DNAT --to-destination "$CONTAINER_IP"
             sudo iptables --table nat --insert POSTROUTING --source "$CONTAINER_IP" --destination 0.0.0.0/0 --jump SNAT --to-source "$EXTERNAL_IP"
+            # set-up NAT rules for MITM
+            sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$EXTERNAL_IP" --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:64462 # incorrect destination (port number)???!?!?!
+            # housekeeping echo statement
             echo "[$(date +'%Y-%m-%d %H:%M:%S')] SUCCESS: nat rules set for "$CONTAINER_NAME"" >> scripts.log
             
             break # stop checking; container has been recycled
