e46a702f3c8a65739aae06bdee0c7a2a596695b9
ljia2178 <76266078+ljia2178@users.noreply.github.com>
2024-10-20 00:50:42 -0400

    fixed container type access

--- grab_logs.sh
+++ grab_logs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ $# -ne 1 ]
 then
         echo "[usage] grab_logs <container_name>"
@@ -8,11 +9,9 @@ fi
 #It also deletes all login/out files associated with it to ensure that mistakes do not occur
 #Keylogger files are left alone because they don't have naming overlap problems
 cont_name=$1
-date_time=`cat /home/student/MITM/logs/logins/$1.log | head -n 1 | cut -d';' -f3`
-date=`echo $date_time | cut -d'_' -f1-3`
-cont_type=`cat ~/.recycle_util_$cont_name | cut -d' ' -f3`
-
-
+date_time=$(cat /home/student/MITM/logs/logins/$1.log | head -n 1 | cut -d';' -f3)
+date=$(echo $date_time | cut -d'_' -f1-3)
+cont_type=$(grep "epoch" ./recycle_util_$cont_name | cut -d ':' -f2)
 
 #saving the authentication log
 sudo mv /home/student/MITM/logs/authentication_attempts/$1.log /home/student/attacker_logs/$date/auth
@@ -21,5 +20,5 @@ sudo mv /home/student/MITM/logs/authentication_attempts/$1.log /home/student/att
 sudo mv /home/student/MITM/logs/session_streams/$date_time.log.gz /home/student/attacker_logs/$date/streams/$cont_type
 
 #moving somewhat less important files
-sudo mv /home/student/MITM/logs/logins/$1.log ~/attacker_logs/$date/logins
-sudo mv /home/student/MITM/logs/logouts/$1.log ~/attacker_logs/$date/logins
+sudo mv /home/student/MITM/logs/logins/$1.log home/student/attacker_logs/$date/logins
+sudo mv /home/student/MITM/logs/logouts/$1.log home/student/attacker_logs/$date/logins
