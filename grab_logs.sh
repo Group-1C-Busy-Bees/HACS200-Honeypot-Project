b6afc2a2a22a5b1fe99f3bb2a7178882f6858a15
ljia2178 <76266078+ljia2178@users.noreply.github.com>
2024-10-22 21:22:58 -0400

    grab_logs overhaul
    fixed a bunch of typos, should work without sudo

--- grab_logs.sh
+++ grab_logs.sh
@@ -11,7 +11,7 @@ fi
 cont_name=$1
 date_time=$(cat /home/student/MITM/logs/logins/$1.log | head -n 1 | cut -d';' -f3)
 date=$(echo $date_time | cut -d'_' -f1-3)
-cont_type=$(grep "epoch" ./recycle_util_$cont_name | cut -d ':' -f2)
+cont_type=$(grep "config" ./recycle_util_$cont_name | cut -d ':' -f2)
 
 #saving the authentication log
 sudo mv /home/student/MITM/logs/authentication_attempts/$1.log /home/student/attacker_logs/$date/auth
@@ -20,5 +20,5 @@ sudo mv /home/student/MITM/logs/authentication_attempts/$1.log /home/student/att
 sudo mv /home/student/MITM/logs/session_streams/$date_time.log.gz /home/student/attacker_logs/$date/streams/$cont_type
 
 #moving somewhat less important files
-sudo mv /home/student/MITM/logs/logins/$1.log home/student/attacker_logs/$date/logins
-sudo mv /home/student/MITM/logs/logouts/$1.log home/student/attacker_logs/$date/logins
+sudo mv /home/student/MITM/logs/logins/$1.log /home/student/attacker_logs/$date/logins
+sudo mv /home/student/MITM/logs/logouts/$1.log /home/student/attacker_logs/$date/logouts
